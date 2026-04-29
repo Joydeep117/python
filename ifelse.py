b9bf61fc952d7cf1de58476ffc38546a0372d380
Joydeep Bera <joydeepbera264@gmail.com>
2026-04-29 09:15:11 +0530

    Add files via upload

---
+++ ifelse.py
@@ -0,0 +1,7 @@
+num = int(input("Enter the number :"))
+if num > 0 :
+    print(" This is a positive number .")
+elif num < 0 :
+    print("This number is a negative  number .")
+else :
+    print(" This number is zero ( 0 ) ")
